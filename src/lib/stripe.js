b512b7d7dce97db2c0a6208af5501b51eafdc910
emmanueljuarezmil <emmanueljuarezmil@gmail.com>
2021-12-23 22:36:42 -0300

    session 2.1

---
+++ src/lib/stripe.js
@@ -0,0 +1,18 @@
+// src/lib/stripe.js
+import * as React from 'react'
+import { loadStripe } from '@stripe/stripe-js'
+
+export function useStripeClientSecret() {
+  const [clientSecret, setClientSecret] = React.useState(null)
+
+  React.useEffect(() => {
+    fetch('/api/create-payment-intent', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret))
+  }, [])
+
+  return clientSecret
+}
